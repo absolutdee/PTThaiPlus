935e7660a29527a219eb362138f043fab84d43ed
absolutdee <149473879+absolutdee@users.noreply.github.com>
2025-07-26 10:58:44 +0700

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+# jest.config.js
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/serviceWorker.js',
+    '!src/setupTests.js',
+    '!src/**/*.stories.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
